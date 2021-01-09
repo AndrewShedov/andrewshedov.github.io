87b2f40348386799fe7e651b4f5a2c5eb454636f
Andrew <andrew.shedov@gmail.com>
2021-01-09 14:41:20 +0300

    commit.

--- hotel/script.js
+++ hotel/script.js
@@ -1,7 +1,7 @@
 let burger = document.getElementById('burger');
 let menu = document.getElementById('menu');
 let wrap = document.getElementById('wrap');
-
+let UpButton = document.getElementById('UpButton');
 
 ///////////////Function opening menu
 function calcShowMenu(showMenu) {
@@ -10,7 +10,8 @@ function calcShowMenu(showMenu) {
     menu.classList.toggle("menuMoveLeft", showMenu);
    
     const menuWidth = menu.offsetWidth;
-    [wrap.style, burger.style].map(v => v.transform = `translateX(${showMenu ? -menuWidth + 1 : 0}px)`);
+    [wrap.style, burger.style, UpButton.style].map(v => v.transform = `translateX(${showMenu ? -menuWidth + 1 : 0}px)`);
+
     [burger.style].map(v => v.transform = `translateX(${showMenu ? -menuWidth - 25 : 0}px)`);
     
     }
@@ -122,6 +123,57 @@ function Tabs() {
 
 
 
+
+/* UP BUTTON */
+
+function scrollTo(to, duration = 700) {
+  const
+      element = document.scrollingElement || document.documentElement,
+      start = element.scrollTop,
+      change = to - start,
+      startDate = +new Date(),
+      // t = current time
+      // b = start value
+      // c = change in value
+      // d = duration
+      easeInOutQuad = function (t, b, c, d) {
+          t /= d / 2;
+          if (t < 1) return c / 2 * t * t + b;
+          t--;
+          return -c / 2 * (t * (t - 2) - 1) + b;
+      },
+      animateScroll = function () {
+          const currentDate = +new Date();
+          const currentTime = currentDate - startDate;
+          element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+          if (currentTime < duration) {
+              requestAnimationFrame(animateScroll);
+          }
+          else {
+              element.scrollTop = to;
+          }
+      };
+  animateScroll();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  let btn = document.querySelector('#UpButton');
+  window.addEventListener('scroll', function () {
+      // If we scrolled further 599px, we show the button
+      if (pageYOffset > 100) {
+          btn.classList.add('show');
+          // Otherwise we hide
+      } else {
+          btn.classList.remove('show');
+      }
+  });
+  
+  // When you click, scroll to the very top
+  btn.onclick = function (click) {
+      click.preventDefault();
+      scrollTo(0, 200);
+  }
+});
 
 
 
