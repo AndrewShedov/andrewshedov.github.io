dcc13855ae2d128080119299b70feafd88c2f159
Andrew <andrew.shedov@gmail.com>
2022-09-14 10:25:58 +0300

    commit.

--- Call_back_popup_on_jQuery/script.js
+++ Call_back_popup_on_jQuery/script.js
@@ -1,34 +1,22 @@
 $(document).ready(function($) {
-	
-	// Сall Back 1
 
-	$('.callback_button_wrap').click(function() {
-	$('.callback_window_wrap').fadeIn(500);
-	$('.callback_button_wrap').fadeOut(300);
-	$('.callback_wrap').fadeIn(500);
-	//return false;
-	});	
-	
-
-	$('.callback_window-button_close').click(function() {
-	$('.callback_window_wrap').fadeOut(500);
-	$('.callback_button_wrap').fadeIn(500);
-	//return false;
-	});		
-
-
-$('.callback_window_wrap').click(function(e) {
-	if (e.target == this){
-		$('.callback_wrap').fadeOut(500);
-   	$('.callback_button_wrap').fadeIn(500);
-	}
-	//return false;
+$('.callback_button_wrap').click(function() {
+$('.callback_window_wrap').fadeIn(500);
+$('.callback_button_wrap').fadeOut(300);
+$('.callback_wrap').fadeIn(500);
 });	
 
+$('.callback_window-button_close').click(function() {
+$('.callback_window_wrap').fadeOut(500);
+$('.callback_button_wrap').fadeIn(500);
+});		
 
-// Сall Back 2
-
-
+$('.callback_window_wrap').click(function(e) {
+if (e.target == this){
+$('.callback_wrap').fadeOut(500);
+$('.callback_button_wrap').fadeIn(500);
+}
+});	
 });
 
 
