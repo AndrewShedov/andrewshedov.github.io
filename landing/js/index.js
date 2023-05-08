8441f59155ecf67f05ef923a32d1c263ecc08448
Andrew <andrew.shedov@gmail.com>
2023-05-09 00:26:35 +0300

    commit.

--- landing/js/index.js
+++ landing/js/index.js
@@ -3,23 +3,23 @@
 
 $("nav ul").hide();
 
-$(".nav-toggle").click( function() {
+$(".nav-toggle").click(function () {
   $("nav ul").slideToggle("medium");
 });
 
-$("nav ul li a, .brand a").click( function() {
+$("nav ul li a, .brand a").click(function () {
   $("nav ul").hide();
 });
 
 ////////////////////////////////////
 // SMOOTH SCROLLING WITH NAV HEIGHT OFFSET
 
-$(function() {
+$(function () {
   var navHeight = $("nav").outerHeight();
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top - navHeight
@@ -36,8 +36,8 @@ $(function() {
 var viewHeight = $(window).height();
 var navigation = $('nav');
 
-$(window).scroll( function() {
-  if ( $(window).scrollTop() > (viewHeight - 175) ) { //edit for nav height
+$(window).scroll(function () {
+  if ($(window).scrollTop() > (viewHeight - 175)) { //edit for nav height
     navigation.addClass('sticky');
   } else {
     navigation.removeClass('sticky');
@@ -51,31 +51,22 @@ function centerSplash() {
   var navHeight = $("nav").outerHeight();
   var splashHeight = $(".splash .container").height();
   var remainingHeight = $(window).height() - splashHeight - navHeight;
-  $(".splash .container").css({"padding-top" : remainingHeight/2, "padding-bottom" : remainingHeight/2});
+  $(".splash .container").css({ "padding-top": remainingHeight / 2, "padding-bottom": remainingHeight / 2 });
 }
 
-$( document ).ready( function() {
+$(document).ready(function () {
   centerSplash();
 });
 
-$( window ).resize( function() {
+$(window).resize(function () {
   centerSplash();
 });
 
-//низ страницы вверх//
-
-
-
-$(window).scroll(function() {
-
-if($(window).scrollTop()+$(window).height()>=$(document).height()){
-  $(".scrollup").css("bottom", "55px");
-}
-
-
-else {
-  $(".scrollup").css("bottom", "10px");
-
- }
-
+$(window).scroll(function () {
+  if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+    $(".scrollup").css("bottom", "55px");
+  }
+  else {
+    $(".scrollup").css("bottom", "10px");
+  }
 });
