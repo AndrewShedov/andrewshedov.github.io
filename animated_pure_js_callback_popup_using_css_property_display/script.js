6a7e1673fc0acaea7ab28050119d8e211c591b2c
Andrew <andrew.shedov@gmail.com>
2023-08-12 11:20:07 +0300

    commit.

--- animated_pure_js_callback_popup_using_css_property_display/script.js
+++ animated_pure_js_callback_popup_using_css_property_display/script.js
@@ -8,26 +8,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
   callback_button_wrap.addEventListener("click", (e) => {
     e.stopPropagation();
-      animateDisplay(
+    animateDisplay(
       document.querySelector(".callback_wrap"),
       "active",
       "block",
       300
     );
-    document.getElementsByClassName('callback_button_wrap')[0].style= "visibility: hidden";
-    document.getElementsByClassName('callback_button_wrap')[0].style= "opacity: 0";
+    document.getElementsByClassName("callback_button_wrap")[0].style =
+      "visibility: hidden";
+    document.getElementsByClassName("callback_button_wrap")[0].style =
+      "opacity: 0";
   });
 
   callback_window_button_close.addEventListener("click", (e) => {
     e.stopPropagation();
-   animateDisplay(
+    animateDisplay(
       document.querySelector(".callback_wrap"),
       "active",
       "block",
       300
     );
-    document.getElementsByClassName('callback_button_wrap')[0].style= "visibility: visible";
-    document.getElementsByClassName('callback_button_wrap')[0].style= "opacity: 1";
+    document.getElementsByClassName("callback_button_wrap")[0].style =
+      "visibility: visible";
+    document.getElementsByClassName("callback_button_wrap")[0].style =
+      "opacity: 1";
   });
 
   // Click outside the callback form
@@ -37,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
       target == callback_window || callback_window.contains(target);
     let target_callback_button_wrap = target == callback_button_wrap;
     let callback_wrap_active = callback_wrap.classList.contains("active");
-     if (
+    if (
       !target_callback_window &&
       !target_callback_button_wrap &&
       callback_wrap_active
@@ -48,8 +52,10 @@ document.addEventListener("DOMContentLoaded", function () {
         "block",
         300
       );
-      document.getElementsByClassName('callback_button_wrap')[0].style= "visibility: visible";
-      document.getElementsByClassName('callback_button_wrap')[0].style= "opacity: 1";
+      document.getElementsByClassName("callback_button_wrap")[0].style =
+        "visibility: visible";
+      document.getElementsByClassName("callback_button_wrap")[0].style =
+        "opacity: 1";
     }
   });
 
@@ -57,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
   function animateDisplay(target, animationClass, displayType, timeout) {
     let doneTimedDisplay = false,
       displaying = false;
-      target.addEventListener("transitionend", function () {
+    target.addEventListener("transitionend", function () {
       if (!target.classList.contains("active")) {
         target.style.display = "none";
       }
@@ -69,12 +75,12 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       displaying = false;
     }
-      setTimeout(function () {
+    setTimeout(function () {
       target.classList.toggle(animationClass);
       doneTimedDisplay = false;
     }, 10);
-        if (!displaying) {
-        setTimeout(function () {
+    if (!displaying) {
+      setTimeout(function () {
         if (!doneTimedDisplay) {
           target.style.display = "none";
         }
