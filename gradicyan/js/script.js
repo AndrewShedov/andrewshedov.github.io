7c01b83d2b461ac048dd668afbd9eb1c4da4e570
Andrew <andrew.shedov@gmail.com>
2023-11-10 07:30:49 +0300

    commit.

--- gradicyan/js/script.js
+++ gradicyan/js/script.js
@@ -32,7 +32,56 @@ jQuery(document).ready(function (a) {
             }
         })
     })
+
+    var lastId,
+    topMenu = $(".nav__item"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       
+       lastId = id;
+       // Set/remove active class
+       menuItems
+       
+         .parent().removeClass("active")
+         
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
 });
- 
-//Slider
- 
+
+
+});
+$(document).ready(function () {
+    $("div[class^=contents]:not(:first)").hide();
+    $(".dropdown").change(function () {
+        value = $(this).find("option:selected").val();
+        $(".dropdown option[value=" + value + "]").removeAttr("disabled");
+        $('div[class^="contents"]').hide();
+        $(".contents" + value).show()
+    })
+});
